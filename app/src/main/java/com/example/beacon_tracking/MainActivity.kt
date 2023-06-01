683c90a611d03fd6315e4bb2ec65913fc6808f80
jiseungmin <sm46384638@naver.com>
2023-06-01 18:22:54 +0900

    Beacon Connect Success

--- app/src/main/java/com/example/beacon_tracking/MainActivity.kt
+++ app/src/main/java/com/example/beacon_tracking/MainActivity.kt
@@ -1,37 +1,48 @@
 package com.example.beacon_tracking
 
-import android.content.ContentValues.TAG
+import android.content.DialogInterface
 import android.content.pm.PackageManager
 import android.os.Bundle
+import android.os.SystemClock
 import android.util.Log
 import android.widget.Button
+import android.widget.Chronometer
 import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
+import androidx.core.view.isVisible
 import androidx.lifecycle.Observer
-import org.altbeacon.beacon.BeaconConsumer
 import org.altbeacon.beacon.BeaconManager
 import org.altbeacon.beacon.BeaconParser
+import org.altbeacon.beacon.Identifier
 import org.altbeacon.beacon.MonitorNotifier
 import org.altbeacon.beacon.Region
 
 class MainActivity : AppCompatActivity() {
-    private var ex  = 0
-    private lateinit var beacon: BeaconManager
+    private var Ex  = 0
+    private lateinit var Beacon: BeaconManager
     private val MY_PERMISSION_ACCESS_ALL = 100
+    private val Timmer : Chronometer by  lazy {findViewById(R.id.Timemmer)}
     private val DetectButton : Button by lazy { findViewById(R.id.DetectButton) }
-    private val stateTextView : TextView by  lazy { findViewById(R.id.stateTextView) }
-    private val region = Region("all-beacons-region", null, null, null)
-
-
+    private val StateTextView : TextView by  lazy { findViewById(R.id.stateTextView) }
+    private val region = Region("beacons", Identifier.parse("74278bda-b644-4520-8f0c-720eaf059935"), null, null) // 비콘정보  식별객체
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        beacon =  BeaconManager.getInstanceForApplication(this)
-        PermissionReq() // 권한 설정
-        ScanButton()
+        Beacon = BeaconManager.getInstanceForApplication(this)
+        Beacon.getBeaconParsers().add(BeaconParser().setBeaconLayout("m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24")) // 비콘파서는 비콘의 정보를 추출하는 역할을 하며 비콘레이아웃은 비콘에서 전송되는 데이터 패턴을 정의하는 역할을 함
+        LocationPermission() // 권한 설정
+        ScanButton() // 버튼
     }
 
+    private fun LocationPermission() {
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            val permissions = arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION)
+            ActivityCompat.requestPermissions(this, permissions, MY_PERMISSION_ACCESS_ALL)
+        }
+    }
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         if (requestCode === MY_PERMISSION_ACCESS_ALL) {
@@ -44,42 +55,49 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun startScan() {
-        beacon.getRegionViewModel(region).regionState.observeForever(monitoringObserver)
-        beacon.startMonitoring(region)
+        Ex = 1
+        Timmer.isVisible = true
+        Timmer.base = SystemClock.elapsedRealtime()
+        Timmer.start()
+        DetectButton.setText("탐색 중지")
+        StateTextView.setText("주변 물체를 찾고 있습니다.")
+        Beacon.getRegionViewModel(region).regionState.observeForever(monitoringObserver) // 비콘 객체 상태 관찰
+        Beacon.startMonitoring(region)
+    }
+    private fun ScanButton(){
+        DetectButton.setOnClickListener {
+            if (Ex == 0) { startScan() }
+            else { showConfirmationDialog() }
+        }
     }
 
     private val monitoringObserver = Observer<Int> { state ->
         if (state == MonitorNotifier.INSIDE) {
-            Log.d(TAG, "Beacons detected")
-            stateTextView.text = "주변 물체가 감지되었습니다. "
+            StateTextView.text = "주변 물체가 감지되었습니다. "
+            Toast.makeText(this, "주변 물체와 거리가 가까워졌습니다.",Toast.LENGTH_SHORT).show()
         }
         else {
-            Log.d(TAG, "No beacons detected")
-            stateTextView.text = "주변 물체를 감지하지 못했습니다."
+            StateTextView.text = "주변 물체를 감지하지 못했습니다."
+            Toast.makeText(this, "주변 물체와 거리가 멀어졌습니다.",Toast.LENGTH_SHORT).show()
         }
     }
 
-    private fun ScanButton(){
-        DetectButton.setOnClickListener {
-            if (ex == 0) {
-                ex = 1
-                DetectButton.setText("탐색 중지")
-                stateTextView.setText("주변 물체를 찾고 있습니다.")
-                startScan()
-            } else {
-                ex = 0
-                DetectButton.setText("탐색 하기")
-                stateTextView.setText("탐지를 진행하시겠습니까?")
-                beacon.stopMonitoring(region)
-            }
-        }
-    }
-
-    private fun PermissionReq(){
-        if(ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED
-            ||ActivityCompat.checkSelfPermission(this,android.Manifest.permission.BLUETOOTH_SCAN)!= PackageManager.PERMISSION_GRANTED){
-            var permissions = arrayOf(android.Manifest.permission.ACCESS_COARSE_LOCATION, android.Manifest.permission.BLUETOOTH_SCAN)
-            ActivityCompat.requestPermissions(this, permissions, MY_PERMISSION_ACCESS_ALL)
-        }
+    private fun showConfirmationDialog() {
+        val alertDialogBuilder = AlertDialog.Builder(this)
+        alertDialogBuilder.setTitle("Stop Detecting")
+        alertDialogBuilder.setMessage("원하는 물체를 발견하였습니까?")
+        alertDialogBuilder.setPositiveButton("예") { dialog, which ->
+            Ex = 0
+            Timmer.stop()
+            Timmer.base = SystemClock.elapsedRealtime()
+            Timmer.isVisible = false
+            DetectButton.setText("탐색 하기")
+            StateTextView.setText("탐지를 진행하시겠습니까?")
+            Beacon.stopMonitoring(region)
+            Beacon.getRegionViewModel(region).regionState.removeObservers(this)
+         }
+        alertDialogBuilder.setNegativeButton("아니오") { dialog, which -> {} }
+        val alertDialog = alertDialogBuilder.create()
+        alertDialog.show()
     }
 }
